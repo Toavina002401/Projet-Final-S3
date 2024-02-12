b7fd0505f8ca972638fe42496f4402ac18ca5cb2
Belouh Andrianina <toavinawukeys@gmail.com>
2024-02-12 18:48:39 +0300

    finition header et footer

--- WEB/pages/Templates/footer.php
+++ WEB/pages/Templates/footer.php
@@ -1,17 +1,79 @@
-</body>
-<footer>
-    <div class="boite-footer">
-        <div class="enfant-footer">
-            <p>ANDRIANAVALONA Fitahiantsoa Heritoavina</p>
-            <p>ETU 002401</p>
-        </div>
-        <div  class="enfant-footer">
-            <p>Antema RAKOTOARIVELO</p>
-            <p>ETU 002415</p>
-        </div>
-        <div  class="enfant-footer">
-            <p>RAZAFITSIALONINA Malala Ninah</p>
-            <p>ETU 00261</p>
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+
+    <div class="footer-top" >
+        <div class="container-fluid">
+            <div class="row gy-4">
+                <div class="col-lg-1">
+
+                </div>
+                <div class="col-lg-5 col-md-12 footer-info">
+                    <a href="index.html" class="logo d-flex align-items-center">
+                    <img src="assets/img/logo.png" alt="">
+                    <span>Théicole</span>
+                    </a>
+                    <p>Les ouvriers, telles des fourmis laborieuses, s'affairent avec dévouement à leur tâche quotidienne, cueillant, triant, séchant avec précision et savoir-faire ancestral. Leurs mains sont le lien entre la terre fertile et la tasse fumante qui réchauffe les cœurs à travers le monde.</p>
+                    <div class="social-links mt-3">
+                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-12 footer-contact text-center text-md-start">
+                    <h4>Nos Informations</h4>
+                    <p>
+                        ITU Andoharanofotsy <br>
+                        Madagascar,Antananarivo 102<br><br>
+                    </p>
+                    <div class="row">
+                        <p class="col-lg-4 col-md-12">                    
+                            <strong>Nom :</strong>Antema RAKOTOARIVELO<br>
+                            <strong>ETU :</strong>2415<br>
+                            <strong>Téléphone:</strong> +261 38 40 708 49<br>
+                            <strong>Email:</strong> antemazazart@gmail.com</br>
+                        </p>
+                        <p class="col-lg-4 col-md-12">                    
+                            <strong>Nom :</strong>RAZAFITSIALONINA Malala Ninah<br>
+                            <strong>ETU :</strong>2631<br>
+                            <strong>Téléphone:</strong> +261 34 99 354 38<br>
+                            <strong>Email:</strong> malalaninah6@gmail.com</br>
+                        </p>
+                        <p class="col-lg-4 col-md-12">                    
+                            <strong>Nom :</strong>ANDRIANAVALONA Fitahiantsoa Heritoavina<br>
+                            <strong>ETU :</strong>2401<br>
+                            <strong>Téléphone:</strong> +261 34 42 075 00<br>
+                            <strong>Email:</strong> toavinawukeys@gmail.com</br>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</footer>
+
+    <div class="container">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Théicole</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        Designed by <a href="">IT-students</a>
+    </div>
+    </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
--- WEB/pages/Templates/header.php
+++ WEB/pages/Templates/header.php
@@ -5,5 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projet Final S3</title>
     <link rel="stylesheet" href="../../assets/css/style.css">
+    
+    <!-- Vendor CSS Files -->
+    <link href="../../assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 </head>
 <body>
